997178f1f3d84a661e2d4b26aaf3215ffe9b0d7e
Daves-Mac <daveswork@users.noreply.github.com>
2017-10-08 18:40:09 -0400

    Completed Week 3 excercise 2.

--- week-3/process_texts/language.py
+++ week-3/process_texts/language.py
@@ -1,5 +1,10 @@
 from collections import Counter
 import numpy as np
+import pandas as pd
+import os
+import matplotlib
+matplotlib.use('qt5agg')
+import matplotlib.pyplot as plt
 
 text = "This is my text text. We're keeping this text short to keep things manageable."
 
@@ -72,4 +77,64 @@ def more_frequent(distribution):
     and counts as keys!
     '''
     counts = list(distribution.keys())
-    return distribution
+    frequency_of_counts = list(distribution.values())
+    sum_of_frequency = np.cumsum(frequency_of_counts)
+    max_of_freq = max(sum_of_frequency)
+    new_ratio = []
+    for i in sum_of_frequency:
+        new_ratio.append(1 - i/float(max_of_freq))
+    return dict(zip(counts, new_ratio))
+
+book_titles = {'English': {'shakespeare': ("A+Midsummer+Night's+Dream",
+   'Hamlet',
+   'Macbeth',
+   'Othello',
+   'Richard+III',
+   'Romeo+and+Juliet',
+   'The+Merchant+of+Venice')},
+ 'French': {'chevalier': ("L'enfer+et+le+paradis+de+l'autre+monde",
+   "L'i%CC%82le+de+sable",
+   'La+capitaine',
+   'La+fille+des+indiens+rouges',
+   'La+fille+du+pirate',
+   'Le+chasseur+noir',
+   'Les+derniers+Iroquois')},
+ 'German': {'shakespeare': ('Der+Kaufmann+von+Venedig',
+   'Ein+Sommernachtstraum',
+   'Hamlet',
+   'Macbeth',
+   'Othello',
+   'Richard+III',
+   'Romeo+und+Julia')},
+ 'Portuguese': {'shakespeare': ('Hamlet',)}}
+
+
+hamlets = pd.DataFrame(columns=["language", "distribution"])
+book_dir = "Books"
+data_filepath = os.getcwd() +"/"
+title_num = 1
+for language in book_titles:
+    for author in book_titles[language]:
+        for title in book_titles[language][author]:
+            if title == "Hamlet":
+                inputfile = data_filepath+"Books/"+language+"/"+author+"/"+title+".txt"
+                text = read_book(inputfile)
+                distribution = word_count_distribution(text)
+                hamlets.loc[title_num] = language, distribution
+                title_num += 1
+
+colors = ["crimson", "forestgreen", "blueviolet"]
+handles, hamlet_languages = [], []
+for index in range(hamlets.shape[0]):
+    language, distribution = hamlets.language[index+1], hamlets.distribution[index+1]
+    dist = more_frequent(distribution)
+    plot, = plt.loglog(sorted(list(dist.keys())),sorted(list(dist.values()),
+        reverse = True), color = colors[index], linewidth = 2)
+    handles.append(plot)
+    hamlet_languages.append(language)
+plt.title("Word Frequencies in Hamlet Translations")
+xlim    = [0, 2e3]
+xlabel  = "Frequency of Word $W$"
+ylabel  = "Fraction of Words\nWith Greater Frequency than $W$"
+plt.xlim(xlim); plt.xlabel(xlabel); plt.ylabel(ylabel)
+plt.legend(handles, hamlet_languages, loc = "upper right", numpoints = 1)
