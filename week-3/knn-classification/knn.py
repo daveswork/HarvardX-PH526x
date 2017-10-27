afc986bf7e08b7de2a52b74282fae1f15c1b9422
Dave J <daveswork@users.noreply.github.com>
2017-10-27 01:36:15 -0400

    Added plt.show(block=True) to be able to see the plot.

--- week-3/knn-classification/knn.py
+++ week-3/knn-classification/knn.py
@@ -88,11 +88,13 @@ points = np.array([[1,1],[1,2],[1,3],[2,1],[2,2],[2,3],[3,1],[3,2],[3,3]])
 p = np.array([2.5,2])
 outcomes = np.array([0,0,0,0,1,1,1,1,1])
 
-plt.interactive(True)
+
+
 plt.plot(points[:,0], points[:,1], "ro")
 plt.plot(p[0], p[1], "bo")
 
 plt.axis([0.5, 3.5, 0.5, 3.5])
 
+plt.show(block=True)
 ind = find_nearest_neighbors(p, points, 4)
 print(knn_predict(p, points,outcomes, k=2))
